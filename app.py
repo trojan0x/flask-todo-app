9f978b153e51909a1fb7b4f7aad7a4c5c4dc6a41
Jeff Dauda <jefdauda@gmail.com>
2022-02-23 14:54:28 -0500

    Templates - index -

--- app.py
+++ app.py
@@ -5,4 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "App running"
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
