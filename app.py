8cf49dbe627e6272db66300b8e51ea134a860bcd
Jeff Dauda <jefdauda@gmail.com>
2022-02-23 19:43:49 -0500

    Added tmpl

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template
 import os
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(300), nullable=False)
+    
+    def __msg__(self):
+        return "<Task %r>" % self.id
+        
+
+
 @app.route("/")
+
 def index():
     return render_template("index.html")
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='127.0.0.1', port=port)
